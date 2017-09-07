0825dec9a0c046e4bf71207bee4b8eade4618be3
djskim27 <djs.kim27@gmail.com>
2017-09-07 17:07:40 -0400

    making my first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FaAdjust, FaFacebookSquare} from 'react-icons/lib/fa'
+import {Button, Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>David's Portfolio</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar inverse collapseOnSelect className='navbar'>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+       
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+
+  
       </div>
     );
   }
